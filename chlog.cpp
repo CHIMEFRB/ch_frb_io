bf48d05221f029d936f849a652b86d3bd233dead
Dustin Lang <dstndstn@gmail.com>
2017-01-24 15:05:22 -0500

    trying to ensure clean exit

--- chlog.cpp
+++ chlog.cpp
@@ -1,4 +1,6 @@
+#include <cstdlib>
 #include <thread>
+#include <pthread.h>
 #include <functional>
 #include <iostream>
 #include <ostream>
@@ -26,9 +28,12 @@ stringprintf(const char* format, ...) {
 class chime_log_socket {
 public:
      chime_log_socket(zmq::context_t &ctx) :
-         //_ctx(ctx),
          _socket(ctx, ZMQ_PUB)
     {}
+
+    ~chime_log_socket() {
+        cout << "~chime_log_socket" << endl;
+    }
 
     void add_server(std::string port) {
         cout << "chime_log connecting to " << port << endl;
@@ -36,24 +41,30 @@ public:
     }
 
     void send(std::string msg) {
-        cout << "chime_log sending message: " << msg << endl;
+        //cout << "chime_log sending message: " << msg << endl;
         size_t n = _socket.send(static_cast<const void*>(msg.data()), msg.size());
-        cout << "sent " << n << endl;
+        //cout << "sent " << n << endl;
     }
 
 protected:
-    //zmq::context_t &_ctx;
-
     zmq::socket_t _socket;
     
 };
 
 static chime_log_socket* logsock = NULL;
 
+void chime_log_quit() {
+    cout << "chime_log_quit" << endl;
+    if (logsock)
+        delete logsock;
+    logsock = NULL;
+}
+
 void chime_log_init(zmq::context_t &ctx) {
     if (logsock)
         delete logsock;
     logsock = new chime_log_socket(ctx);
+    std::atexit(chime_log_quit);
 }
 
 void chime_log_add_server(string port) {
@@ -128,36 +139,58 @@ static string msg_string(zmq::message_t &msg) {
 
 std::mutex cout_mutex;
 
+static void* server_main(void* varg) {
+    std::tuple<zmq::context_t*, string> *args = reinterpret_cast<std::tuple<zmq::context_t*, string>* >(varg);
+    zmq::context_t* ctx = std::get<0>(*args);
+    string port = std::get<1>(*args);
 
-static void server_main(zmq::context_t* ctx, string port) {
     zmq::socket_t sock(*ctx, ZMQ_SUB);
     sock.setsockopt(ZMQ_SUBSCRIBE, "", 0);
     sock.bind(port);
     cout << "server_main: bound " << port << endl;
     for (;;) {
         zmq::message_t msg;
-        cout << "server_main: waiting to receive message." << endl;
-        sock.recv(&msg);
+        //cout << "server_main: waiting to receive message." << endl;
+        try {
+            if (!sock.recv(&msg)) {
+                cout << "log server: failed to receive message" << endl;
+                break;
+            }
+        } catch (const zmq::error_t& e) {
+            cout << "log server: error receiving message: " << e.what() << endl;
+            break;
+        }
 
         {
             std::lock_guard<std::mutex> lock(cout_mutex);
             cout << "Server received message: " << msg_string(msg) << endl;
         }
     }
+    cout << "server_main done." << endl;
+    return NULL;
 }
 
 int main() {
     zmq::context_t ctx;
 
-    string port = "tcp://127.0.0.1:6666";
-    thread serverthread(std::bind(server_main, &ctx, port));
-
-    string port2 = "tcp://127.0.0.1:6667";
-    thread serverthread2(std::bind(server_main, &ctx, port2));
-
     chime_log_init(ctx);
+
+    /*
+    string port = "tcp://127.0.0.1:6666";
+    //thread serverthread(std::bind(server_main, &ctx, port));
+    pthread_t serverthread;
+    std::tuple<zmq::context_t*, string> args(&ctx, port);
+    if (pthread_create(&serverthread, NULL, server_main, &args)) {
+        cout << "Failed to create server thread." << endl;
+        return -1;
+    }
     chime_log_add_server(port);
-    chime_log_add_server(port2);
+    //serverthread.detach();
+     */
+
+    // string port2 = "tcp://127.0.0.1:6667";
+    // thread serverthread2(std::bind(server_main, &ctx, port2));
+    // chime_log_add_server(port2);
 
     usleep(1000000);
 
@@ -166,6 +199,12 @@ int main() {
     chdebug("Debug" << 42+43);
 
     usleep(1000000);
+    //ctx.close();
+
+    chime_log_quit();
+
+    cout << "main() finished" << endl;
+    return 0;
 }
 
 
